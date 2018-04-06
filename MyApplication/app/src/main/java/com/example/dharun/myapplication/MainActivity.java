5971d14581142c91b7c88e0fba05885918946175
Dharun Luka Das <b5011507@my.shu.ac.uk>
2018-04-06 10:59:59 +0100

    app take 1
    this is my initial attempt at the app

---
+++ MyApplication/app/src/main/java/com/example/dharun/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.dharun.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+
+}
